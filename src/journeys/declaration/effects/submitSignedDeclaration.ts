9b74fd98024678d7ba596dd6090c3724b4555e58
James Grant <James.Grant@justice.gov.uk>
2026-08-18 09:36:38 +0100

    feat: populate effect to update declaration

--- src/journeys/declaration/effects/submitSignedDeclaration.ts
+++ src/journeys/declaration/effects/submitSignedDeclaration.ts
@@ -1,4 +1,9 @@
+import { ApiResponseError } from "#/api/clients/api.errors.js";
+import { getRcwApiDefaultOptions } from "#/api/clients/getRcwApiDefaultOptions.js";
+import { updateApplicationDeclaration } from "#/api/clients/rcw/schema/applications/applications.gen.js";
+import { getAuthDebugHeaders } from "#/auth/auth.debug.js";
 import { AnswerKey as A } from "#/journeys/AnswerKey.js";
+import { HTTP_STATUS } from "#/lib/constants/http.js";
 import { logger } from "#/logger.js";
 
 import type {
@@ -12,18 +17,51 @@ import type {
  * @returns void
  */
 export const submitSignedDeclaration =
-  (deps: DeclarationDeps) => (context: DeclarationContext) => {
+  (deps: DeclarationDeps) => async (context: DeclarationContext) => {
+    const session = context.getSession();
     const applicationId = context.getRequestParam("applicationId");
     const confirmed = context.getAnswer(A.DECLARATION_SIGNED_CONFIRM);
     const date = context.getAnswer(A.DECLARATION_SIGNED_DATE);
 
-    logger.error(`applicationId: ${applicationId}`);
-    logger.error(`confirmed: ${confirmed}`);
-    logger.error(`date: ${date}`);
+    if (!session) {
+      logger.error("Missing session");
+      throw new Error("Missing session");
+    }
 
-    // TODO Need an endpoint in the RCW API to update the application with the signed declaration.
+    if (!applicationId) {
+      logger.error("Missing applicationId in request parameters");
+      throw new Error("Missing applicationId in request parameters");
+    }
+
+    let response;
+
+    try {
+      const body = {
+        dateSigned: date,
+        declarationConfirmation: confirmed === "yes",
+      };
+
+      const opts = await getRcwApiDefaultOptions({
+        homeAccountId: session.msal?.homeAccountId,
+        sessionId: session.id,
+      });
+
+      response = await updateApplicationDeclaration(applicationId, body, opts);
+    } catch (error) {
+      logger.error("Failed to update application declaration", error, {
+        api: "updateApplicationDeclaration",
+      });
+      throw ApiResponseError.from(error);
+    }
+
+    if (response.status !== HTTP_STATUS.NO_CONTENT) {
+      const error = new ApiResponseError();
+      logger.error("createApplication did not return 204", error, {
+        api: "updateApplicationDeclaration",
+        authHeaders: getAuthDebugHeaders(response.headers),
+        data: null,
+        status: response.status,
+      });
+      throw error;
+    }
   };
-
-export const validateSignedDeclaration = (): boolean => {
-  return false;
-};
