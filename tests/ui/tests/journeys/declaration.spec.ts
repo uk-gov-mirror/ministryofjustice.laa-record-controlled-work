37d4f10e0c021b9bbbd2c3e92cd4498370a33ef3
James Grant <James.Grant@justice.gov.uk>
2026-08-19 15:56:22 +0100

    tests: add playwright test for declaration sign

--- tests/ui/tests/journeys/declaration.spec.ts
+++ tests/ui/tests/journeys/declaration.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from "../../fixtures/index.js";
 import { application } from "../../msw/fixtures/rcw.fixtures.js";
 
-test("Client Declaration - Client Declaration step", async ({ withSelectedOffice: page }) => {
-    const applicationId = application.id;
-  
+test("Client Declaration - Client Declaration step", async ({
+  withSelectedOffice: page,
+}) => {
+  const applicationId = application.id;
+
   // ==========================================================================
   // Client Declaration page
   // ==========================================================================
@@ -36,4 +38,43 @@ test("Client Declaration - Client Declaration step", async ({ withSelectedOffice
 
   await page.getByRole("button", { name: "Confirm and continue" }).click();
   await expect(page).toHaveURL(`/cases/${applicationId}/declaration/sign`);
+});
+
+test("Client Declaration - Client Declaration step", async ({
+  withSelectedOffice: page,
+}) => {
+  const applicationId = application.id;
+
+  // ==========================================================================
+  // Client Declaration page
+  // ==========================================================================
+
+  await page.goto(`/cases/${applicationId}/declaration/sign`);
+
+  await expect(
+    page.getByRole("heading", {
+      name: "Sign the declaration",
+      level: 1,
+    }),
+  ).toBeVisible();
+
+  await expect(
+    page.getByRole("button", {
+      name: "Continue",
+    }),
+  ).toBeVisible();
+
+  await expect(
+    page.getByRole("button", {
+      name: "Save and return later",
+    }),
+  ).toBeVisible();
+
+  await page.getByRole("button", { name: "Save and return" }).click();
+  await expect(page).toHaveURL(`/cases/${applicationId}/declaration/confirm/`);
+
+  await page.goto(`/cases/${applicationId}/declaration/sign`);
+
+  await page.getByRole("button", { name: "Continue" }).click();
+  await expect(page).toHaveURL(`/cases/${applicationId}/declaration/ufn`);
 });
