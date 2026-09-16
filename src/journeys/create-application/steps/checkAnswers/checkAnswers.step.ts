8709b1e5d6553507b59cbbceb84fdea37bb3e447
Andrew Clayton <105248663+DrewIW@users.noreply.github.com>
2026-09-16 13:22:52 +0100

    Update src/journeys/create-application/steps/checkAnswers/checkAnswers.step.ts
    Co-authored-by: James Grant <James.Grant@justice.gov.uk>

--- src/journeys/create-application/steps/checkAnswers/checkAnswers.step.ts
+++ src/journeys/create-application/steps/checkAnswers/checkAnswers.step.ts
@@ -16,6 +16,7 @@ import { StepCode } from "#/journeys/StepCode.enum.js";
 import { t } from "#/lib/i18n.js";
 
 const CHECK_ANSWERS = t("journeys.createApplication.checkAnswers.title");
+
 /**
  * Creates the check-answers step for the create-application journey.
  *
